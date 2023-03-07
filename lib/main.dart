fabb6deb419940a388af98e46269548bbd0dbe36
beksultanrrr <ramazanov.beksultan1902@gmail.com>
2023-03-06 23:20:38 -0800

    autorization

--- lib/main.dart
+++ lib/main.dart
@@ -34,44 +34,180 @@ class _VideoAppState extends State<VideoApp> {
     return MaterialApp(
       title: 'Video Demo',
       home: Scaffold(
-          body: Stack(children: <Widget>[
-        VideoPlayer(_controller),
-        Center(
-          child: Container(
-            // margin: EdgeInsets.all(32.0),
-
-            child: Container(
-              color: Colors.black,
-              padding: EdgeInsets.symmetric(horizontal: 199, vertical: 119),
-              height: 600,
-              width:700,
-              child: Column(
+          resizeToAvoidBottomInset: false,
+          body: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+            VideoPlayer(_controller),
+            Expanded(
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                // mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "977777",
-                        filled: true,
-                        fillColor: Colors.white),
+                  Container(
+                    margin: EdgeInsets.only(top: 25.0),
+                    padding: EdgeInsets.only(top: 34.0),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(20.0)),
+                      color: Color.fromRGBO(39, 39, 39, 1),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(padding: EdgeInsets.only(top: 5)),
+                        Text(
+                          "Cheflist",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontFamily: "cera-round-pro2"),
+                        ),
+                        SizedBox(height: 48.0),
+                        Text(
+                          "Авторизация",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontFamily: "cera-round-pro2"),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 10)),
+                        Text(
+                          "Пожалуйста выберите способ авторизации",
+                          style: TextStyle(
+                            color: Color.fromRGBO(159, 170, 174, 1),
+                            fontSize: 12,
+                            fontFamily: "cera-round-pro2",
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 30)),
+                        Row(
+                          children: [
+                            Padding(padding: EdgeInsets.only(left: 15)),
+                            const SizedBox(
+                              width: 75,
+                              height: 50,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: "",
+                                    filled: true,
+                                    fillColor: Color.fromRGBO(54, 54, 54, 1)),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.only(left: 15)),
+                            const SizedBox(
+                              width: 293,
+                              height: 50,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: "+971(__)__-__-__",
+                                    filled: true,
+                                    fillColor: Color.fromRGBO(54, 54, 54, 1)),
+                                style: TextStyle(
+                                    fontSize: 19,
+                                    fontFamily: "cera-round-pro2",
+                                    color: Color.fromARGB(255, 255, 255, 255)),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 30)),
+                        SizedBox(
+                            width: 380,
+                            height: 45,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Отправить",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(54, 54, 54, 1),
+                                  fontSize: 15,
+                                  fontFamily: "cera-round-pro2",
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30))),
+                            )),
+                        Padding(padding: EdgeInsets.only(top: 50)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MaterialButton(
+                              onPressed: () {},
+                              color: Color.fromRGBO(54, 54, 54, 1),
+                              textColor: Colors.white,
+                              child: Icon(
+                                Icons.apple,
+                                size: 40,
+                              ),
+                              padding: EdgeInsets.all(5),
+                              shape: CircleBorder(),
+                            ),
+                            MaterialButton(
+                              onPressed: () {},
+                              color: Color.fromRGBO(54, 54, 54, 1),
+                              textColor: Color.fromARGB(255, 45, 3, 230),
+                              child: Icon(
+                                Icons.facebook_sharp,
+                                size: 40,
+                              ),
+                              padding: EdgeInsets.all(5),
+                              shape: CircleBorder(),
+                            ),
+                            MaterialButton(
+                              onPressed: () {},
+                              color: Color.fromRGBO(54, 54, 54, 1),
+                              textColor: Colors.white,
+                              child: Icon(
+                                Icons.apple,
+                                size: 40,
+                              ),
+                              padding: EdgeInsets.all(5),
+                              shape: CircleBorder(),
+                            ),
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20)),
+                        TextButton(
+                            onPressed: () {},
+                            child: RichText(
+                                text: TextSpan(
+                                    style: TextStyle(fontSize: 13),
+                                    children: <TextSpan>[
+                                  TextSpan(
+                                      text: "или ",
+                                      style: TextStyle(
+                                          color: Color.fromRGBO(
+                                              159, 170, 174, 1))),
+                                  TextSpan(
+                                      text: "продолжить как гость",
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(159, 170, 174, 1),
+                                          decoration: TextDecoration.underline))
+                                ])))
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: const Image(
+                      image: NetworkImage(
+                          "https://img.freepik.com/premium-vector/chef-logo-vector_20448-270.jpg"),
+                      height: 50,
+                      width: 50,
+                      alignment: Alignment(0, -1.3),
+                    ),
                   ),
                 ],
-                // children: <Widget>[
-                // TextField(
-                //   decoration: InputDecoration(
-                //     border: OutlineInputBorder(),
-                //     hintText: "977777",
-                //     contentPadding: EdgeInsets.symmetric(horizontal: 70,),
-                //     filled: true,
-                //     fillColor: Colors.white
-                //   ),
-                // ),
-
-                // ],
               ),
-            ),
-          ),
-        )
-      ])),
+            )
+          ])),
     );
   }
 
